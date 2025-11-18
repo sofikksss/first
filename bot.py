28f1b3aabe3a12521ff905e0df12f6d4d652b727
sofikksss <sofiakorzova614@gmail.com>
2025-11-18 19:38:29 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,8 @@
+while True:
+    command = input("Введите команду: ")
+    if command == "пошути":
+        print("*смешная шутка*")
+    elif command == "поздоровайся":
+        print("привет)")
+    else:
+        print("я не знаю такой команды(")
