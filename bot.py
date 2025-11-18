97060df0c79251baabb19a14a423c84165eeea62
Sofiia Korzhova <sofiakorzova614@gmail.com>
2025-11-18 19:56:08 +0200

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-while True:
-    command = input("Введите команду: ")
-    if command == "пошути":
-        print("*смешная шутка*")
-    elif command == "поздоровайся":
-        print("привет)")
-    else:
-        print("я не знаю такой команды(")
